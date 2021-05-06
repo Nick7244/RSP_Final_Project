6c0d257c4032817bda25ec3fdbdc5547f9c1ed05
Seena Vafaee <seena.vafaee@gmail.com>
2021-05-06 03:04:11 -0400

    working on ball frame transformation

--- ball_position_transformer/src/ball_position_transformer.cpp
+++ ball_position_transformer/src/ball_position_transformer.cpp
@@ -1,6 +1,12 @@
 #include <ros/ros.h>
 #include <ur5_kendama_msgs/ball_position.h>
 #include <geometry_msgs/TransformStamped.h>
+#include <geometry_msgs/PointStamped.h>
+
+#include <tf2/convert.h>
+#include <tf2/LinearMath/Quaternion.h>
+#include <tf2/LinearMath/Transform.h>
+#include <kdl/frames.hpp>
 
 ur5_kendama_msgs::ball_position world_ball_pos_msg;
 geometry_msgs::TransformStamped camera_to_world;
@@ -10,6 +16,17 @@ void ballPosCallback(const ur5_kendama_msgs::ball_position& camera_ball_pos) {
 
   //Apply transformation to ball position in camera frame
   //TODO
+  geometry_msgs::PointStamped camera_ball_point;
+  camera_ball_point.point.x = camera_ball_pos.x;
+  camera_ball_point.point.y = camera_ball_pos.y;
+  camera_ball_point.point.z = camera_ball_pos.z;
+
+  geometry_msgs::PointStamped world_ball_point;
+  tf2::doTransform(camera_ball_point, world_ball_point, camera_to_world);
+
+  world_ball_pos_msg.x = world_ball_point.point.x;
+  world_ball_pos_msg.y = world_ball_point.point.y;
+  world_ball_pos_msg.z = world_ball_point.point.z;
 
   //Publish ball position in world frame
   pub.publish(world_ball_pos_msg);
